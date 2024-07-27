a2caf91f2082e9277acbfdcc9d1fe88f978a9fbe
Anton Kapranov <gr8toad@gmail.com>
2024-07-27 09:34:54 +0300

    Приложение готово к ревью. Градиент пока не готов, нужно подумать над лучшим решением + подумать про скругления

--- Image Feed/Utility/GradientFormater.swift
+++ Image Feed/Utility/GradientFormater.swift
@@ -1,5 +1,17 @@
 import UIKit
 
+extension UIView {
+    func addGradiend (colors: [CGColor]) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = colors
+        gradientLayer.cornerRadius = layer.cornerRadius
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+        gradientLayer.frame = bounds
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
+
 //extension CAGradientLayer {
 //    func configureGradient(colors: [UIColor], locations: [NSNumber]? = nil, startPoint: CGPoint = CGPoint(x: 0.5, y: 0.0), endPoint: CGPoint = CGPoint(x: 0.5, y: 1.0)) {
 //        self.colors = colors.map { $0.cgColor }
@@ -17,11 +29,15 @@ import UIKit
 //        layer.frame = frame
 //        return layer
 //    }
-//    
+//
 //    private static func colors() -> [CGColor] {
 //        let beginColor: CGColor = UIColor(named:"YP Black")!.cgColor
 //        let endColor: CGColor = UIColor(named:"YP Black (Alpha 0)")!.cgColor
-//        
+//
 //        return [beginColor, endColor]
 //    }
+//}
+
+//extension CAGradientLayer {
+//    private static func Colors ()
 //}
