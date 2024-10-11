ed70f727fe791f1f27fcce6e30a5a6f461e91d68
Anton Kapranov <gr8toad@gmail.com>
2024-10-11 03:07:10 +0300

    Подготовка к добавлению scroll view на SinglrImageViewController

--- Image Feed/SingleImageViewController/SingleImageViewController.swift
+++ Image Feed/SingleImageViewController/SingleImageViewController.swift
@@ -8,4 +8,7 @@ final class SingleImageViewController:UIViewController {
         super.viewDidLoad()
         imageView.image = image
     }
+    @IBAction func didTapBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
 }
--- Image Feed/Utility/CornerRadius.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CornerRadius.swift
-//  Image Feed
-//
-//  Created by Антон Капранов on 26.07.2024.
-//
-
-import Foundation
