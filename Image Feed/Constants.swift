c200485b0fb8990a5c5ce12b6553699f078d590b
Anton Kapranov <gr8toad@gmail.com>
2024-10-20 21:13:14 +0300

    Added API Keys in Config file

---
+++ Image Feed/Constants.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+enum Constants {
+    static let acessKey = "yN-WpvRvC3m8qrJjfq6UyCfh7Hmni8TsWqgp8C9AtvY"
+    static let secretKey = "2URFEG8mGRxpQtPzCq8XwwIJnhC7viHKbahbVch2fJQ"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
