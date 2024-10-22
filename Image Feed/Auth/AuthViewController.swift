121c19766573c03fc517fea69e46824f6e32b357
Anton Kapranov <gr8toad@gmail.com>
2024-10-22 08:50:31 +0300

    Сделал открытие веб страницы логина через web view

---
+++ Image Feed/Auth/AuthViewController.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+final class AuthViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Image Feed/Constants.swift
+++ Image Feed/Constants.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 enum Constants {
-    static let acessKey = "yN-WpvRvC3m8qrJjfq6UyCfh7Hmni8TsWqgp8C9AtvY"
+    static let accessKey = "yN-WpvRvC3m8qrJjfq6UyCfh7Hmni8TsWqgp8C9AtvY"
     static let secretKey = "2URFEG8mGRxpQtPzCq8XwwIJnhC7viHKbahbVch2fJQ"
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
