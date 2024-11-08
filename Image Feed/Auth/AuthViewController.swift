8e05dca5565b6c663ff842bb0cfb7f01708f18b8
Anton Kapranov <gr8toad@gmail.com>
2024-11-09 02:17:57 +0300

    WebViewController dismiss button

--- Image Feed/Auth/AuthViewController.swift
+++ Image Feed/Auth/AuthViewController.swift
@@ -2,12 +2,9 @@ import UIKit
 
 final class AuthViewController: UIViewController {
     private let showWebViewSegueIdentifier = "ShowWebView"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        view.backgroundColor = .systemPink
-        
-    }
-    @IBAction func didTappedBack(_ sender: Any) {
-        dismiss(animated: true)
     }
 }
+
--- Image Feed/Auth/WebViewViewController.swift.swift
+++ Image Feed/Auth/WebViewViewController.swift.swift
@@ -1,15 +1,14 @@
 import UIKit
 import WebKit
 
-enum WebViewConstants {
-    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
-}
-
-final class WebViewViewController: UIViewController {
+final class WebViewController: UIViewController {
     @IBOutlet weak var webView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .ypWhite_1
         webView.backgroundColor = .ypWhite_1
-        
+    }
+    @IBAction func didTappedBackButton(_ sender: Any) {
+        dismiss(animated: true)
     }
 }
