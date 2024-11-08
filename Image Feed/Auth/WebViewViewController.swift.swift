7ec63162d8bbc826be21a0081cc2a04db79e9c96
Anton Kapranov <gr8toad@gmail.com>
2024-11-09 02:23:54 +0300

    added load of WLWebView

--- Image Feed/Auth/WebViewViewController.swift.swift
+++ Image Feed/Auth/WebViewViewController.swift.swift
@@ -1,14 +1,42 @@
 import UIKit
 import WebKit
 
+enum WebViewConstants {
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+}
+
 final class WebViewController: UIViewController {
     @IBOutlet weak var webView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .ypWhite_1
-        webView.backgroundColor = .ypWhite_1
+        loadAuthView()
     }
     @IBAction func didTappedBackButton(_ sender: Any) {
         dismiss(animated: true)
     }
+}
+
+extension WebViewController {
+    private func loadAuthView() {
+        view.backgroundColor = .ypWhite_1
+        webView.backgroundColor = .ypWhite_1
+        
+        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else {
+            return
+        }
+
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: Constants.accessScope)
+        ]
+
+        guard let url = urlComponents.url else {
+            return
+        }
+
+        let request = URLRequest(url: url)
+        webView.load(request)
+        }
 }
