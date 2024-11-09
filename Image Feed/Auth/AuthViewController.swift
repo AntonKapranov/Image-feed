2a55876af1c3dbfac94d27b1e310df0bbc1d06de
Anton Kapranov <gr8toad@gmail.com>
2024-11-10 02:36:37 +0300

    Done with Delegate error (Call of Nil)

--- Image Feed/Auth/AuthViewController.swift
+++ Image Feed/Auth/AuthViewController.swift
@@ -1,10 +1,34 @@
 import UIKit
+import WebKit
+
+protocol AuthViewControllerDelegate: AnyObject {
+    func authViewController(_ vc: AuthViewController, didAuthenticateWithCode: String)
+}
 
 final class AuthViewController: UIViewController {
+    weak var delegate: AuthViewControllerDelegate?
     private let showWebViewSegueIdentifier = "ShowWebView"
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showWebViewSegueIdentifier {
+            guard let webViewViewController = segue.destination as? WebViewViewController
+            else { fatalError("Failed to prepare for \(showWebViewSegueIdentifier)") }
+            webViewViewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
     }
 }
 
+extension AuthViewController: WebViewViewControllerDelegate {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+    }
+    
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        print("Delegate has been called")
+        dismiss(animated: true)
+    }
+}
+
+//i f*n hate storyboards. I had developed it by code in minutes, but spend days in trying to resolve a Nil in Delegate's call. This is my last ever App in Storyboards - @got_sin
--- Image Feed/Auth/WebViewViewController.swift.swift
+++ Image Feed/Auth/WebViewViewController.swift.swift
@@ -1,71 +1,123 @@
 import UIKit
-import WebKit
+@preconcurrency import WebKit
 
 enum WebViewConstants {
     static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
 }
 
-final class WebViewController: UIViewController {
-    @IBOutlet weak var webView: WKWebView!
-    webView.navigationDelegate = self
+protocol WebViewViewControllerDelegate: AnyObject {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String)
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController)
+}
+
+final class WebViewViewController: UIViewController {
+    
+    weak var delegate: WebViewViewControllerDelegate?
+    
+    @IBOutlet private weak var webView: WKWebView!
+    @IBOutlet private weak var progressView: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadAuthView()
+        webView.navigationDelegate = self
+        loadWebView()
+        updateProgress()
     }
-    @IBAction func didTappedBackButton(_ sender: Any) {
-        dismiss(animated: true)
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        addObserver()
+        updateProgress()
+    }
+    
+    @IBAction private func tapBackButton(_ sender: Any?) {
+        delegate?.webViewViewControllerDidCancel(self)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        addObserver()
+        updateProgress()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), context: nil)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        webView.removeObserver(
+            self,
+            forKeyPath: #keyPath(WKWebView.estimatedProgress),
+            context: nil
+        )
     }
 }
 
-extension WebViewController {
-    private func loadAuthView() {
-        view.backgroundColor = .ypWhite_1
-        webView.backgroundColor = .ypWhite_1
-        
-        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else {
-            return
-        }
-
-        urlComponents.queryItems = [
+extension WebViewViewController {
+    func loadWebView() {
+        var components = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString)!
+        components.queryItems = [
             URLQueryItem(name: "client_id", value: Constants.accessKey),
             URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
             URLQueryItem(name: "response_type", value: "code"),
             URLQueryItem(name: "scope", value: Constants.accessScope)
         ]
-
-        guard let url = urlComponents.url else {
-            return
-        }
-
+        let url = components.url!
         let request = URLRequest(url: url)
         webView.load(request)
-        }
+    }
     
     private func code(from navigationAction: WKNavigationAction) -> String? {
-        if
-            let url = navigationAction.request.url,
-            let urlComponents = URLComponents(string: url.absoluteString),
-            urlComponents.path == "/oauth/authorize/native",
-            let items = urlComponents.queryItems,
-            let codeItem = items.first(where: { $0.name == "code" })
-        {
+        if let url = navigationAction.request.url,
+           let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false),
+           let codeItem = urlComponents.queryItems?.first(where: { $0.name == "code" }) {
             return codeItem.value
-        } else {
-            return nil
         }
+        return nil
     }
 }
-
-extension WebViewController:WKNavigationDelegate {
+extension WebViewViewController: WKNavigationDelegate {
     func webView(
         _ webView: WKWebView,
-            decidePolicyFor navigationAction: WKNavigationAction,
-            decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
-    ){
+        decidePolicyFor navigationAction: WKNavigationAction,
+        decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
+    ) {
         if let code = code(from: navigationAction) {
+            delegate?.webViewViewController(self, didAuthenticateWithCode: code)
             decisionHandler(.cancel)
         } else {
             decisionHandler(.allow)
         }
     }
 }
+
+extension WebViewViewController {
+    private func addObserver() {
+        webView.addObserver(
+            self,
+            forKeyPath: #keyPath(WKWebView.estimatedProgress),
+            options: .new,
+            context: nil)
+    }
+    
+    override func observeValue(
+        forKeyPath keyPath: String?,
+        of object: Any?,
+        change: [NSKeyValueChangeKey : Any]?,
+        context: UnsafeMutableRawPointer?
+    ) {
+        if keyPath == #keyPath(WKWebView.estimatedProgress) {
+            updateProgress()
+        } else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
+    }
+    
+    private func updateProgress() {
+        progressView.progress = Float(webView.estimatedProgress)
+        progressView.isHidden = fabs(webView.estimatedProgress - 1.0) <= 0.0001
+    }
+}
+
+
